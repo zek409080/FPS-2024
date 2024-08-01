4ca1f91aff1c254155b3bc6256c41be8d57a843c
zaikodan <zaikodan@gmail.com>
2024-08-01 08:12:09 -0300

    GameManager e Player prefab

---
+++ FPS 2024/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    #region Singleton
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+    const string playerPrefabPath = "Prefabs/Player";
+
+    int playersInGame;
+    List<PlayerController> playerList = new List<PlayerController>();
+    PlayerController playerLocal;
+
+    private void Start()
+    {
+        photonView.RPC("AddPlayer", RpcTarget.AllBuffered);
+    }
+
+    private void CreatePlayer()
+    {
+        PlayerController player = PhotonNetwork.Instantiate(playerPrefabPath, new Vector3(30, 1, 30), Quaternion.identity).GetComponent<PlayerController>();
+    }
+
+    [PunRPC]
+    private void AddPlayer()
+    {
+        playersInGame++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+        {
+            CreatePlayer();
+        }
+    }
+}
